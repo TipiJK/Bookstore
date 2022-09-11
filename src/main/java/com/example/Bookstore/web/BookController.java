78fa8030857d1071135001ce9117b05f6902b9d5
Tiina Karppinen <karppinentj@gmail.com>
2022-09-11 15:05:39 +0300

    added ADD and DELETE functionalities

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -3,8 +3,11 @@ package com.example.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 
 @Controller
@@ -16,5 +19,21 @@ public class BookController {
     public String bookList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long studentId, Model model) {
+    	repository.deleteById(studentId);
+    	return "redirect:../booklist";
+    }
+    
+    @RequestMapping(value="/add")
+    public String addBook(Model model) {
+    	model.addAttribute("book", new Book());
+    	return "addbook";
+    }
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public String saveBook(Book book) {
+    	repository.save(book);
+    	return "redirect:booklist";
     }
 }
